a38899fee443643e655a4b66ea8d18cd8cabf2b2
NSarvari <nabilasarwary@abv.bg>
2022-12-09 12:39:39 +0200

    DbContext Added

---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using Movies.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory{ get; set; }
+    }
+}
