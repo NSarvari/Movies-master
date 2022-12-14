6109a1b15a6f264ad3254226b38ecdf57cbaae45
Nabila Sarvari <nabilasarwary@abv.bg>
2022-12-14 11:36:33 +0200

    DbContext_Updated

--- Movies/Data/AppDbContext.cs
+++ Movies/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Movies.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,34 @@ namespace Movies.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MovieActor>().HasKey(am => new
+            {
+                am.ActorID,
+                am.MovieID
+            });
+
+            modelBuilder.Entity<MovieActor>().HasOne(m => new
+            {
+                m.MovieID
+            });
+
+
+            modelBuilder.Entity<MovieActor>().HasOne(a => new
+            {
+                a.ActorID
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //Seting Models
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
